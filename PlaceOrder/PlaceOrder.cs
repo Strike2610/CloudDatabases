2fb81f32d074e885737e84b3debc4184ac05ab93
Tymo Wessel <t.wessel@betabit.nl>
2024-09-16 15:22:56 +0200

    Add project files.

---
+++ PlaceOrder/PlaceOrder.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace PlaceOrder
+{
+    public class PlaceOrder
+    {
+        private readonly ILogger<PlaceOrder> _logger;
+
+        public PlaceOrder(ILogger<PlaceOrder> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("PlaceOrder")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
